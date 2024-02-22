e8f6373e5bd8d262790ab65a470d216fed88963c
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2024-02-22 19:23:07 +0000

    removed words module, artefact from string-patterns

--- src/enums.rs
+++ src/enums.rs
@@ -1,24 +1,3 @@
-
-/// Defines the start, end and both bounds of a word
-pub enum WordBounds {
-  None,
-  Start,
-  End,
-  Both,
-}
-
-impl WordBounds {
-  /// Convert word bounds 
-  pub fn to_pattern(&self, word: &str) -> String {
-    match self {
-      WordBounds::Start => [r#"\b"#, word].concat(),
-      WordBounds::End => [word, r#"\b"#].concat(),
-      WordBounds::Both => [r#"\b"#, word, r#"\b"#].concat(),
-      _ => word.to_owned(),
-    }
-  }
-}
-
 /// Defines the matching bounds of simple string matches with case-insensitive/sensitive variants
 /// and accepting the string pattern and positivity flag as arguments
 #[derive(Debug, Clone)]
